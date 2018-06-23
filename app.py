2bef60ffa708540d2c8a5927e934875970889b42
nteske <nemanjatesic96@gmail.com>
2018-06-23 02:03:32 +0200

    more settings

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import sys,os
 from project import app
 
-if __name__ == "__main__":
-	app.run()
+
+if __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
